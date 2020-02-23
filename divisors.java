8689a762da1f9d0c8614321fa264e2ea4cbbeb8f
SUMYAK JAIN <3456gaurav@gmail.com>
2020-02-23 16:04:12 +0530

    count Divisors

---
+++ divisors.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class divisors{
+ public static void main(String args[]){
+ Scanner sc=new Scanner(System.in);
+     int a=sc.nextInt();
+     int b=sc.nextInt();
+     int c=sc.nextInt();
+     int count=0;
+     for(int i=a;i<=b;i++){
+         if(i%c==0){
+             count++;
+         }
+     }
+     
+     System.out.println(count);
+ }   
+}
