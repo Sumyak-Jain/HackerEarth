92f52634045ffe57621bf9fdc5f2345fd580d656
SUMYAK JAIN <3456gaurav@gmail.com>
2020-02-24 22:33:26 +0530

    Palindromic String

---
+++ palindromic.java
@@ -0,0 +1,17 @@
+import java.io.*; 
+import java.util.Scanner;
+public class palindromic{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        StringBuffer sb=new StringBuffer(a);
+        sb.reverse();
+        String s2 = sb.toString(); 
+        if(a.equals(s2)){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
