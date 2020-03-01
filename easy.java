b15535c84a096e835b9619607ad6a358adf728af
SUMYAK JAIN <3456gaurav@gmail.com>
2020-03-01 12:55:33 +0530

    I am Easy

---
+++ easy.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class easy{
+    public static void main(String args[]){
+        
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    for(int i=1;i<=10;i++){
+        System.out.println(a*i);
+    }
+}
+}
