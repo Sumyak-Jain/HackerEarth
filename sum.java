25b1d16a88f57ebd1f55aa6aaddc1e904171594c
SUMYAK JAIN <3456gaurav@gmail.com>
2020-03-03 17:17:27 +0530

    Char Sum

---
+++ sum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class sum{
+    public static void main(String args[]){
+          char arr[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        int n=a.length();
+        int p=arr.length;
+        int sum=0;
+        char arr1[]=a.toCharArray();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<p;j++){
+                if(arr1[i]==arr[j]){
+                    sum=sum+(j+1);
+                }
+            }
+            
+        }
+    System.out.println(sum);
+               
+    }
+}
