c9a16db3f3f23a5b4c56fb261545d185df609317
SUMYAK JAIN <3456gaurav@gmail.com>
2020-03-14 22:18:22 +0530

    char sum

--- sum.java
+++ sum.java
@@ -7,6 +7,7 @@ public class sum{
         int n=a.length();
         int p=arr.length;
         int sum=0;
+        
         char arr1[]=a.toCharArray();
         for(int i=0;i<n;i++){
             for(int j=0;j<p;j++){
