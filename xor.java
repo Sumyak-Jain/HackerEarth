84764de28538dffd787acba2538e44401f38f5ad
SUMYAK JAIN <3456gaurav@gmail.com>
2020-04-13 22:44:43 +0530

    Update xor.java

--- xor.java
+++ xor.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 public class xor
 {
-public static void main(String args[]){
+public static void main(String args[])
+{
        Scanner sc=new Scanner(System.in);
         int A=sc.nextInt();
        
