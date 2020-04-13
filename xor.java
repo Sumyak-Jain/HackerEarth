7f0a94d4acda2b3994ccb84aeedc93fb1009f1e9
SUMYAK JAIN <3456gaurav@gmail.com>
2020-04-13 22:46:04 +0530

    Update xor.java

--- xor.java
+++ xor.java
@@ -6,7 +6,6 @@ public static void main(String args[])
    Scanner sc=new Scanner(System.in);
       int A=sc.nextInt();
         int max=0;
-        
         int f=0;
         int B=A;
         int C=0;
